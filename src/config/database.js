8dfb3a63cc76fd437c2c6f69e6cac6e49c4c2dd1
Enilton Rastele <eniltjr1@gmail.com>
2019-07-09 20:10:06 -0300

    Error: No Sequelize instance passed

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: "postgres",
+  host: "hansken.db.elephantsql.com",
+  username: "nwzwyewa",
+  password: "Fe5q5gFag--IQVJcTNeoVD_7s-vEHjxu",
+  database: "nwzwyewa",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
