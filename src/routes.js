1e84fe724ce60d8320d4109860e373243b6291a0
Enilton Rastele <eniltjr1@gmail.com>
2020-03-29 23:14:59 -0300

    incluir avatar de provider

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,26 @@
 import { Router } from "express";
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from "./app/middlewares/auth";
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post("/users", UserController.store);
 routes.post("/sessions", SessionController.store);
 
-// routes.user(authMiddleware);
+ routes.use(authMiddleware);
 // a declaração acima, define que este middleware será chamado para todas as rotas
 // abaixo dessa linha:
 
-routes.put("/users", authMiddleware, UserController.update);
+//routes.put("/users", authMiddleware, UserController.update);
+routes.put('/users',UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
